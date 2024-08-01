acaae65a4c175f68778563665f7813dab79ca42b
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-01 12:45:18 +0500

    echo

---
+++ 01_intro.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Hello world</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello world";
+        ?>
+    </body>
+</html>
+
+
+
+<!-- // <?php
+//     echo "Hello World";
+// ?> -->
