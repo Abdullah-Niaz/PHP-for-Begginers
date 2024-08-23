44315840843a0ffa1a52158b3b9704d7b37a11b5
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-23 16:40:10 +0500

    if else

---
+++ 03_if.php
@@ -0,0 +1,12 @@
+<?php
+$age= 20;
+if ($age > 15) {
+    echo "Your age is less than 20";
+}
+else if ($age == 20){
+    echo "your age is 20";
+}
+else{
+    echo "Your age is greater than 20";
+}
+?>
